65ed17bcbfc91ac814826c4b2a31d2f7bda24b0e
Spindel Ljungmark <ljungmark@modio.se>
2023-05-25 14:50:07 +0200

    fixup! WIP: Start cluging in the message log so we can see what it should look like.

--- chirpstack/src/downlink/tx_ack.rs
+++ chirpstack/src/downlink/tx_ack.rs
@@ -42,35 +42,17 @@ pub struct TxAck {
 impl TxAck {
     pub async fn handle(tx_ack: gw::DownlinkTxAck) {
         let span = span!(Level::TRACE, "tx_ack", downlink_id = tx_ack.downlink_id);
-        let mut elog = messagelog::LogEntryBuilder::new()
-            .log_source(messagelog::Endpoint::Gateway)
-            .source_id(&tx_ack.gateway_id)
-            .log_destination(messagelog::Endpoint::Local)
-            .our_desitnation_id()
-            .build();
-
         if let Err(e) = TxAck::_handle(tx_ack).instrument(span).await {
             error!(error = %e, "Handling tx ack error");
-            elog.frame_status.error_desc = e.to_string();
+            //elog.frame_status.error_desc = e.to_string();
         }
         else {
-            elog.frame_status.result = messagelog::FrameStatusResult::OK;
+            // elog.frame_status.result = messagelog::FrameStatusResult::OK;
         }
-        // TODO: Spindel
-        //  elog.tx_ack.phy_payload ....
-        // TODO: Spindel
-        //    elog.tx_ack.downlink_txinfo = self.downlink_frame_item.tx_info
-        //      elog.dev_eui = ...
-        //      elog.knwon_device = true 
-        //      elog.frame_status = ....  OK
-        //      
-        if self.phy_payload.is_some()
-        if let  Some(frame_item) = &self.downlink_frame_item {
-            elog.tx_ack = Some(messagelog::TxPacket {
-
-
-        }
-        messagelog::send(elog).await;
+        // TODO: Spindel: This is the wrong place as we do not see the final event in this
+        // function
+        // Actual log would be good to wrap into either the span to get it with the "instrument"
+        // data, or passed through the ::_handle function.
     }
 
     async fn _handle(tx_ack: gw::DownlinkTxAck) -> Result<()> {
